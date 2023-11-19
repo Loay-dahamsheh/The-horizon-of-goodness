c46db754204a4f492b7b51b26e7d9565d964eb6d
Loay-dahamsheh <dahamshehloay@gmail.com>
2023-11-20 01:11:06 +0200

    loay

---
+++ server/Routes/beneficiariesRoute.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const beneficiariesController = require('../Controllers/beneficiariesController');
+const middleware = require("../Middleware/authorization")
+
+router.post("/newrequest",middleware.authorize,beneficiariesController.newRequest);
+router.get("/allrequest",middleware.authorize,beneficiariesController.getAllRequest);
+router.put("/deleterequest/:id",middleware.authorize,beneficiariesController.deleteRequest);
+router.put("/updatestatus/:id",middleware.authorize,beneficiariesController.updateRequeststatus);
+router.put("/updateaccept/:id",middleware.authorize,beneficiariesController.updateRequestaccept);
+router.get("/allrequestAdmin",beneficiariesController.getAllRequestAdmin);
+
+
+
+
+
+
+
+module.exports = router;
